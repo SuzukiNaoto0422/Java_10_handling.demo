809cdb508b8ceee44867d8aeb51c043c54ace814
SuzukiNaoto0422 <naojin.naojin@gmail.con>
2023-02-14 11:27:56 +0900

    ageカラムの追加

--- sql/001-users.sql
+++ sql/001-users.sql
@@ -3,6 +3,6 @@ DROP TABLE IF EXISTS users;
 CREATE TABLE `users` (
   `id` int UNSIGNED NOT NULL AUTO_INCREMENT,
   `name` varchar(20) COLLATE utf8mb4_General_ci NOT NULL,
-  `age` int UNSIGNED NOT NULL AUTO_INCREMENT,
+  `age` int UNSIGNED NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_General_ci;
