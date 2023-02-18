9a4c4edae34e3e09a12969847c11303f1e7ada26
SuzukiNaoto0422 <naojin.naojin@gmail.con>
2023-02-18 15:30:39 +0900

    内容の追記

--- src/test/java/com/raisetecch/handling/demo/UserMapperTest.java
+++ src/test/java/com/raisetecch/handling/demo/UserMapperTest.java
@@ -1,7 +1,38 @@
 package com.raisetecch.handling.demo;
 
-public class UserMapperTest {
-    void すべての要素を取得できているか() {
+import com.github.database.rider.core.api.dataset.DataSet;
+import com.github.database.rider.spring.api.DBRider;
+import com.raisetecch.handling.demo.entity.User;
+import com.raisetecch.handling.demo.mapper.UserMapper;
+import org.junit.jupiter.api.Test;
+import org.mybatis.spring.boot.test.autoconfigure.MybatisTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+
+@DBRider
+@MybatisTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+public class UserMapperTest {
+
+    @Autowired
+    UserMapper userMapper;
+
+    @Test
+    @DataSet(value = "datasets/users.yml")
+    @Transactional
+    void 指定したidでユーザーのすべての要素を取得できているか() {
+        List<User> users = userMapper.findById();
+        assertThat(users)
+                .hazSize(3)
+                .contains(
+                        new User(1, "suzuki", 30),
+                        new User(2, "satou", 25),
+                        new User(3, "takahashi", 18)
+                );
     }
 }
