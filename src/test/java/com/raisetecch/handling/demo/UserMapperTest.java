bb5f4047f44f8ce9be9c801e49787f3f60a23abb
SuzukiNaoto0422 <naojin.naojin@gmail.con>
2023-02-18 13:44:30 +0900

    テストに使用するクラスの作成

---
+++ src/test/java/com/raisetecch/handling/demo/UserMapperTest.java
@@ -0,0 +1,7 @@
+package com.raisetecch.handling.demo;
+
+public class UserMapperTest {
+    void すべての要素を取得できているか() {
+
+    }
+}
---
+++ src/test/java/com/raisetecch/handling/demo/UserServiceTest.java
@@ -0,0 +1,35 @@
+package com.raisetecch.handling.demo;
+
+import com.raisetecch.handling.demo.entity.User;
+import com.raisetecch.handling.demo.mapper.UserMapper;
+import com.raisetecch.handling.demo.service.UserService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+public class UserServiceTest {
+
+    @InjectMocks
+    UserService userService;
+
+    @Mock
+    UserMapper userMapper;
+
+    @Test
+    public void 存在するユーザーのIDを指定されたときに正常にユーザーを返す() throws Exception {
+        doReturn(Optional.of(new User(1, "suzuki", 30))).when(userMapper).findById(1);
+        verify(userMapper, times(1)).findById(1);
+        User actual = userService.findUser(1);
+        assertThat(actual).isEqualTo(new User(1, "suzuki", 30));
+    }
+}
