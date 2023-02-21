727d6273a9a0a44090923684997fee4a8916484b
SuzukiNaoto0422 <naojin.naojin@gmail.con>
2023-02-21 10:57:43 +0900

    コンストラクタの追加

--- src/main/java/com/raisetecch/handling/demo/entity/User.java
+++ src/main/java/com/raisetecch/handling/demo/entity/User.java
@@ -20,6 +20,13 @@ public class User {
             this.name = name;
         }
 
+    public User(int id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+
     public int getId() {
         return id;
     }
