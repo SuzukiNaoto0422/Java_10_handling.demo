792a2750357e0f64ace2a0b9a70e81a35f23fc0d
SuzukiNaoto0422 <naojin.naojin@gmail.con>
2023-02-24 18:28:37 +0900

    POSTメソッドのdbテストコードの実装

--- src/test/java/com/raisetecch/handling/demo/mapper/UserMapperTest.java
+++ src/test/java/com/raisetecch/handling/demo/mapper/UserMapperTest.java
@@ -10,6 +10,7 @@ import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabas
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.hibernate.validator.internal.util.Contracts.assertTrue;
 
 import java.util.Optional;
 
@@ -26,8 +27,23 @@ class UserMapperTest {
     @Transactional
     void 指定したidでユーザーのすべての要素を取得できていること() {
         int id = 1;
+
         Optional<User> user = userMapper.findById(id);
         Assertions.assertTrue(user.isPresent());
         assertThat(user.get()).isEqualTo(new User(1, "suzuki", 30));
     }
+
+    @Test
+    @DataSet(value = "datasets/users.yml")
+    @Transactional
+    void 入力したnameとageで新規ユーザーの登録ができていること() {
+        String name = "sasaki";
+        int age = 25;
+
+        userMapper.registryUser(name, age);
+        Optional<User> newUser = userMapper.findById(4);
+        assertThat(newUser.get()).isEqualTo(new User(4, "sasaki", 25));
+    }
+
+
 }
