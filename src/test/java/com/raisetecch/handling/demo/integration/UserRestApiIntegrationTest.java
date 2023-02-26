1e26824e910de9b25866dba2a44db3a8de392089
SuzukiNaoto0422 <naojin.naojin@gmail.con>
2023-02-26 17:55:13 +0900

    内容の修正

--- src/test/java/com/raisetecch/handling/demo/integration/UserRestApiIntegrationTest.java
+++ src/test/java/com/raisetecch/handling/demo/integration/UserRestApiIntegrationTest.java
@@ -2,23 +2,15 @@ package com.raisetecch.handling.demo.integration;
 
 import com.github.database.rider.core.api.dataset.DataSet;
 import com.github.database.rider.spring.api.DBRider;
-import com.raisetecch.handling.demo.entity.User;
 import org.junit.jupiter.api.Test;
-import org.skyscreamer.jsonassert.JSONAssert;
-import org.skyscreamer.jsonassert.JSONCompareMode;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.nio.charset.StandardCharsets;
-
-import static org.junit.Assert.assertEquals;
 
 @SpringBootTest
 @AutoConfigureMockMvc
